ac9b2f9461bb89e6e47e1816d43abbc2a1b7500c
Greg Bowler <greg.bowler@g105b.com>
2019-03-19 22:55:41 +0000

    Implement most functionality for directory synchronisation

--- src/DirectorySync.php
+++ src/DirectorySync.php
@@ -3,6 +3,7 @@ namespace Gt\Sync;
 
 use FilesystemIterator;
 use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
 
 class DirectorySync extends AbstractSync {
 	const COMPARE_FILEMTIME = 1<<0;
@@ -10,27 +11,175 @@ class DirectorySync extends AbstractSync {
 
 	const DEFAULT_SETTINGS = self::COMPARE_FILEMTIME;
 
+	protected $copiedFiles;
+	protected $skippedFiles;
+	protected $deletedFiles;
+
 	public function __construct(string $source, string $destination) {
 		parent::__construct($source, $destination);
 
 		if(!is_dir($source)) {
 			throw new SyncException("Source directory does not exist: $source");
 		}
+
+		$this->copiedFiles = [];
+		$this->skippedFiles = [];
+		$this->deletedFiles = [];
 	}
 
 	public function exec(int $settings = self::DEFAULT_SETTINGS):void {
 		$this->checkSettings($settings);
 
-		$directory = new RecursiveDirectoryIterator(
+		$iteratorSettings = FilesystemIterator::SKIP_DOTS
+			| FilesystemIterator::KEY_AS_PATHNAME
+			| FilesystemIterator::CURRENT_AS_FILEINFO;
+
+		$sourceIterator = new RecursiveDirectoryIterator(
 			$this->source,
-			FilesystemIterator::SKIP_DOTS |
-			FilesystemIterator::KEY_AS_PATHNAME |
-			FilesystemIterator::CURRENT_AS_FILEINFO
+			$iteratorSettings
+		);
+		$destinationIterator = new RecursiveDirectoryIterator(
+			$this->destination,
+			$iteratorSettings
 		);
 
-		foreach($directory as $file) {
-			var_dump($file);die();
+		$iterator = new RecursiveIteratorIterator($destinationIterator);
+		foreach($iterator as $pathName => $file) {
+			$relativePath = substr(
+				$pathName,
+				strlen($this->destination) + 1
+			);
+
+			if(!$this->sourceFileExists($relativePath)) {
+				$this->delete($relativePath);
+				$this->deletedFiles []= $relativePath;
+			}
 		}
+
+		$iterator = new RecursiveIteratorIterator($sourceIterator);
+		foreach($iterator as $pathName => $file) {
+			$relativePath = substr(
+				$pathName,
+				strlen($this->source) + 1
+			);
+
+			$filesAreIdentical = $this->compareSourceDestination(
+				$relativePath,
+				$settings
+			);
+
+			if($filesAreIdentical) {
+				$this->skippedFiles []= $relativePath;
+				continue;
+			}
+
+			$this->copy($relativePath);
+			$this->copiedFiles []= $relativePath;
+		}
+	}
+
+	public function getCopiedFilesList():array {
+		return $this->copiedFiles;
+	}
+
+	public function getSkippedFilesList():array {
+		return $this->skippedFiles;
+	}
+
+	public function getDeletedFilesList():array {
+		return $this->deletedFiles;
+	}
+
+	protected function copy(string $relativePath):void {
+		$sourceFile = implode(DIRECTORY_SEPARATOR, [
+			$this->source,
+			$relativePath,
+		]);
+		$destinationFile = implode(DIRECTORY_SEPARATOR, [
+			$this->destination,
+			$relativePath,
+		]);
+
+		if(is_dir($sourceFile)) {
+			return;
+		}
+
+		if(!is_dir(dirname($destinationFile))) {
+			mkdir(
+				dirname($destinationFile),
+				0775,
+				true
+			);
+		}
+
+		if(!copy($sourceFile, $destinationFile)) {
+			throw new SyncException("Error copying $sourceFile to $destinationFile.");
+		}
+
+		touch($destinationFile, filemtime($sourceFile));
+	}
+
+	protected function delete(string $relativePath):void {
+		$destinationFile = implode(DIRECTORY_SEPARATOR, [
+			$this->destination,
+			$relativePath,
+		]);
+
+		if(is_dir($destinationFile)) {
+			if(!rmdir($destinationFile)) {
+				throw new SyncException("Error deleting directory: $destinationFile");
+			}
+		}
+		else {
+			if(!unlink($destinationFile)) {
+				throw new SyncException("Error deleting file: $destinationFile");
+			}
+		}
+	}
+
+	protected function sourceFileExists(string $relativePath):bool {
+		$sourceFile = implode(DIRECTORY_SEPARATOR, [
+			$this->source,
+			$relativePath,
+		]);
+
+		return file_exists($sourceFile);
+	}
+
+	protected function compareSourceDestination(
+		string $relativePath,
+		int $settings
+	):bool {
+		$comparatorFunction = null;
+
+		if($settings & self::COMPARE_FILEMTIME) {
+			$comparatorFunction = "filemtime";
+		}
+		if($settings & self::COMPARE_HASH) {
+			$comparatorFunction = "md5_file";
+		}
+
+		$sourceFile = implode(DIRECTORY_SEPARATOR, [
+			$this->source,
+			$relativePath,
+		]);
+		$destinationFile = implode(DIRECTORY_SEPARATOR, [
+			$this->destination,
+			$relativePath,
+		]);
+
+		if(!file_exists($destinationFile)) {
+			return false;
+		}
+
+		if(is_dir($destinationFile)) {
+			return true;
+		}
+
+		$sourceComp = $comparatorFunction($sourceFile);
+		$destinationComp = $comparatorFunction($destinationFile);
+
+		return $sourceComp === $destinationComp;
 	}
 
 	protected function checkSettings(int $settings):void {
